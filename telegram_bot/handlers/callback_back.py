4ac79342604b192320accdf9ac91759234d2ec7e
asamarka-625 <mix0x@mail.ru>
2025-12-22 17:34:58 +0300

    fix bug for back

--- telegram_bot/handlers/callback_back.py
+++ telegram_bot/handlers/callback_back.py
@@ -25,20 +25,17 @@ async def back_callback_run(callback_query: CallbackQuery):
         data["media"] = f"{cfg.IMAGES_DIR}/main/{cfg.MAIN_USER_PHOTO}"
 
     elif back == "map":
-        data["text"] = "Выберите карту из списка"
-        data["keyboard"] = await create_maps_inline()
+        data["text"], data["keyboard"] = await create_maps_inline()
 
     elif back.startswith("category:"):
-        data["text"] = "Выберите категорию из списка"
         map_id = int(back.replace("category:", ""))
-        data["keyboard"] = await create_categories_inline(map_id=map_id)
+        data["text"], data["keyboard"] = await create_categories_inline(map_id=map_id)
 
     elif back.startswith("cards:"):
-        data["text"] = "Выберите карточку из списка"
         map_category_id = callback_query.data.replace("cards:", "")
         map_id, category_id = map(int, map_category_id.split(":"))
 
-        data["keyboard"] = await create_cards_inline(map_id=map_id, category_id=category_id)
+        data["text"], data["keyboard"] = await create_cards_inline(map_id=map_id, category_id=category_id)
 
         map_image = await sql_get_map_image(map_id=map_id)
         data["media"] = f"{cfg.IMAGES_DIR}/maps/{map_image}"
