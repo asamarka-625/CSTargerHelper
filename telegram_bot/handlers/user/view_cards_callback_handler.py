f0209a8e95b2e46a6c023d3767e6f6f3a9391926
asamarka-625 <mix0x@mail.ru>
2025-12-23 10:47:24 +0300

    search view image card 2.0

--- telegram_bot/handlers/user/view_cards_callback_handler.py
+++ telegram_bot/handlers/user/view_cards_callback_handler.py
@@ -114,7 +114,10 @@ async def choice_card_callback_run(callback_query: CallbackQuery):
         )
         text_answer = f"Карточка {card.name}"
 
-    except:
+    except Exception as err:
+        import traceback
+        traceback.print_exc()
+        cfg.logger.error(f"Error: {err}")
         text_answer = "Ошибка выбора карточки"
 
     await callback_query.answer(
