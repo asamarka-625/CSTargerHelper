d263241ebd2aa91e6d9c53af9e6e8c11d4149705
asamarka-625 <mix0x@mail.ru>
2025-12-25 18:17:44 +0300

    search error slides

--- telegram_bot/handlers/user/view_cards_callback_handler.py
+++ telegram_bot/handlers/user/view_cards_callback_handler.py
@@ -178,6 +178,7 @@ async def navigation_card_callback_run(callback_query: CallbackQuery, bot: Bot):
             share_link=share_link
         )
 
+        cfg.logger.info(f"Название: <b>{caption_split[3].replace("Название: ", "").strip()}</b>\n")
         caption = "\n".join(caption_split[4:-1])
         text = (
             f"- Изображение {order}/{max_image}\n\n"
